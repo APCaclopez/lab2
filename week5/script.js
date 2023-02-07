a71e76a199eb6ff327042d9757c384ce4c21678b
Aliyah Kirstie Lopez <aclopez@student.apc.edu.ph>
2023-02-07 14:56:05 +0800

    JS Lesson 22 - HTML DOM (DOM Elements)

--- week5/script.js
+++ week5/script.js
@@ -5,3 +5,7 @@ function displayDate() {
 var messageArray = ["i am aliyah kirstie."];
 var textPosition = 0;
 var speed = 100;
+
+typewriter = () => {
+	document.querySelector("#text").innerHTML = messageArray[0].substring(0,
+	textPosition) + "<span>\u25ae</span>";
