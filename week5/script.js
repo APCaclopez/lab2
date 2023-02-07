dab37d93b5baad2103ab512d90228d13478a00df
Aliyah Kirstie Lopez <aclopez@student.apc.edu.ph>
2023-02-07 15:11:07 +0800

    JS Lesson 23 - Browser DOM

--- week5/script.js
+++ week5/script.js
@@ -9,3 +9,13 @@ var speed = 100;
 typewriter = () => {
 	document.querySelector("#text").innerHTML = messageArray[0].substring(0,
 	textPosition) + "<span>\u25ae</span>";
+
+	if (textPosition++ != messageArray[0].length)
+		setTimeout(typewriter, speed);
+}
+
+window.addEventListener("load", typewriter)
+
+function portfolioAlert() {
+	alert('This section is not available yet.');
+}
